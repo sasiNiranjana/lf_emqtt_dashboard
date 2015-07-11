3c9924670ce84e8f6534103f824e2051a12917bd
huangdan <wang_yi20081014@163.com>
2015-07-11 23:03:18 +0800

    topic tree

--- priv/www/script/emqttd.js
+++ priv/www/script/emqttd.js
@@ -324,6 +324,7 @@ function Topic() {
 	// 加载模块
 	loading('topic.html', function() {
 		_t._topic();
+		_t._loadTree();
 	});
 }
 
@@ -358,6 +359,52 @@ Topic.prototype = {
 		jQuery.ajax(options);
 	},
 
+	_loadTree : function() {
+		var width = 960, height = 2000;
+
+		var tree = d3.layout.tree()
+		    .size([height, width - 160]);
+
+		var diagonal = d3.svg.diagonal()
+		    .projection(function(d) { return [d.y, d.x]; });
+
+		var svg = d3.select("body").append("svg")
+		    .attr("width", width)
+		    .attr("height", height)
+		    .append("g")
+		    .attr("transform", "translate(40,0)");
+	
+		d3.json("http://bl.ocks.org/mbostock/raw/4063550/flare.json", function(error, json) {
+		  if (error) throw error;
+	
+		  var nodes = tree.nodes(json),
+		      links = tree.links(nodes);
+	
+		  var link = svg.selectAll("path.link")
+		      .data(links)
+		    .enter().append("path")
+		      .attr("class", "link")
+		      .attr("d", diagonal);
+	
+		  var node = svg.selectAll("g.node")
+		      .data(nodes)
+		    .enter().append("g")
+		      .attr("class", "node")
+		      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+	
+		  node.append("circle")
+		      .attr("r", 4.5);
+	
+		  node.append("text")
+		      .attr("dx", function(d) { return d.children ? -8 : 8; })
+		      .attr("dy", 3)
+		      .attr("text-anchor", function(d) { return d.children ? "end" : "start"; })
+		      .text(function(d) { return d.name; });
+		});
+	
+		d3.select(self.frameElement).style("height", height + "px");
+	}
+	
 	// 关闭任务（定时任务等）
 	closeTask : function() {
 		
