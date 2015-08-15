74d18c3336e6fbcc0c6258890cd1ec01c3255e6a
huangdan <huangd@opengoss.com>
2015-08-15 21:45:22 +0800

    remove mb

--- priv/www/script/emqttd.js
+++ priv/www/script/emqttd.js
@@ -245,7 +245,7 @@ Overview.prototype = {
 								+ lis['process_used']+ ' / ' + lis['process_available'] + '</td><td>'
 								+ lis['load1'] + ' / ' +  lis['load5'] + ' / ' + lis['load15']
 								+ '</td><td>'
-								+ lis['used_memory'] + 'MB / ' + lis['total_memory'] + 'MB</td><td>' + lis['max_fds'] + '</td></tr>');
+								+ lis['used_memory'] + ' / ' + lis['total_memory'] + '</td><td>' + lis['max_fds'] + '</td></tr>');
 					}
 				}
 			},
