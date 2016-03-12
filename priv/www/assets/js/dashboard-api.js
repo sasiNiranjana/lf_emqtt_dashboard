df28224010ecb3d2f2ebc01add9435be8071598f
huangdan <wang_yi20081014@163.com>
2016-03-12 13:48:38 +0800

    icon

--- priv/www/assets/js/dashboard-api.js
+++ priv/www/assets/js/dashboard-api.js
@@ -170,15 +170,15 @@
 				document.execCommand("ClearAuthenticationCache");
 			}
 			// Firefox实现注销功能  
-			else if (isFirefox = navigator.userAgent.indexOf("Firefox") > 0) {
-				xmlHttp.open("GET", ".force_logout_offer_login_mozilla", true,
+			else if (navigator.userAgent.indexOf("Firefox") > 0) {
+				xmlHttp.open("GET", "/", true,
 						"logout", "logout");
 				xmlHttp.send("");
 				xmlHttp.abort();
 			}
 			// Google Chrome等浏览器实现注销功能
 			else {
-				xmlHttp.open("GET", "./", false, "logout", "logout");
+				xmlHttp.open("GET", "/", false, "logout", "logout");
 				xmlHttp.send(null);
 				xmlHttp.abort();
 			}
