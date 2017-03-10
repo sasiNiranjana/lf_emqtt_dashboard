17932c16343eac27697cfe1d2b90ad83d94c7e79
黃丹 <huangdan@huangdandeMacBook-Pro.local>
2017-03-10 14:27:13 +0800

    Upgrade the copyright info

--- src/emq_dashboard_sup.erl
+++ src/emq_dashboard_sup.erl
@@ -1,5 +1,5 @@
 %%--------------------------------------------------------------------
-%% Copyright (c) 2015-2017 Feng Lee <feng@emqtt.io>.
+%% Copyright (c) 2015-2017 EMQ Enterprise, Inc. (http://emqtt.io).
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -32,8 +32,6 @@ start_link() ->
 init([]) ->
     {ok, { {one_for_all, 10, 100},
            [?CHILD(emq_dashboard_admin)
-%%           ?CHILD(emq_meter_access),
-%%           ?CHILD(emq_meter_define)
            ]
          } }.
 
