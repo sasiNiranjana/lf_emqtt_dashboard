0338e149268b7e118c7decd75f2654b0b9d07861
Feng Lee <ery.lee@gmail.com>
2017-03-23 19:29:09 +0800

    Local_content to avoid blocking on mnesia:wait_for_tables/2

--- src/emq_dashboard_admin.erl
+++ src/emq_dashboard_admin.erl
@@ -131,6 +131,7 @@ init([]) ->
     % Create mqtt_admin table
     ok = emqttd_mnesia:create_table(mqtt_admin, [
                 {type, set},
+                {local_content, true}, %% local_content to avoid blocking on mnesia:wait_for_tables/2
                 {disc_copies, [node()]},
                 {record_name, mqtt_admin},
                 {attributes, record_info(fields, mqtt_admin)}]),
